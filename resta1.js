8802d70ad21e4f6d133a695e1ae5102f9bc98e88
Jose-Carlos00 <josecarlos.dias@estudante.ufjf.br>
2025-08-17 13:23:34 -0300

    resta1 modificado com a logica e estado do jogo

--- resta1.js
+++ resta1.js
@@ -1,3 +1,4 @@
+const LARGURA_TABULEIRO = 7;
 const tabuleiroResta1 = [
     'invalido', 'invalido', 'peca',    'peca',    'peca',    'invalido', 'invalido',
     'invalido', 'invalido', 'peca',    'peca',    'peca',    'invalido', 'invalido',
@@ -7,6 +8,54 @@ const tabuleiroResta1 = [
     'invalido', 'invalido', 'peca',    'peca',    'peca',    'invalido', 'invalido',
     'invalido', 'invalido', 'peca',    'peca',    'peca',    'invalido', 'invalido'
 ];
+let pecaSelecionada = null;
 export function getTabuleiro() {
     return [...tabuleiroResta1];
 }
+export function getPecaSelecionada() {
+    return pecaSelecionada;
+}
+function moverPeca(origem, destino) {
+    if (tabuleiroResta1[origem] !== 'peca' || tabuleiroResta1[destino] !== 'vazio') {
+        return false;
+    }
+    const diff = Math.abs(origem - destino);
+    const pecaComidaPos = (origem + destino) / 2;
+    const movimentoHorizontal = diff === 2 && Math.floor(origem / LARGURA_TABULEIRO) === Math.floor(destino / LARGURA_TABULEIRO);
+    const movimentoVertical = diff === (2 * LARGURA_TABULEIRO);
+    if (!movimentoHorizontal && !movimentoVertical) {
+        return false;
+    }
+    if (tabuleiroResta1[pecaComidaPos] !== 'peca') {
+        return false;
+    }
+    tabuleiroResta1[origem] = 'vazio';
+    tabuleiroResta1[destino] = 'peca';
+    tabuleiroResta1[pecaComidaPos] = 'vazio';
+    return true;
+}
+export function gerenciaClique(posicao) {
+    if (pecaSelecionada === null) {
+        if (tabuleiroResta1[posicao] === 'peca') {
+            pecaSelecionada = posicao;
+            return true;
+        }
+    } else {
+        if (pecaSelecionada === posicao) {
+            pecaSelecionada = null;
+            return true;
+        }
+        const moveu = moverPeca(pecaSelecionada, posicao);
+        if (moveu) {
+            pecaSelecionada = null;
+            return true;
+        } else {
+            if (tabuleiroResta1[posicao] === 'peca') {
+                pecaSelecionada = posicao;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+//José Carlos
