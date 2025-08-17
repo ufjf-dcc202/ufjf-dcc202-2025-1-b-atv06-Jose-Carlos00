3b5411aa9d0cd42ad48393b163446b16894ff884
Jose-Carlos00 <josecarlos.dias@estudante.ufjf.br>
2025-08-16 23:17:55 -0300

    css para o titulo, começo do resta1 para o array

---
+++ resta1.js
@@ -0,0 +1,12 @@
+const tabuleiroResta1 = [
+    'invalido', 'invalido', 'peca',    'peca',    'peca',    'invalido', 'invalido',
+    'invalido', 'invalido', 'peca',    'peca',    'peca',    'invalido', 'invalido',
+    'peca',     'peca',     'peca',    'peca',    'peca',    'peca',     'peca',
+    'peca',     'peca',     'peca',    'vazio',   'peca',    'peca',     'peca',
+    'peca',     'peca',     'peca',    'peca',    'peca',    'peca',     'peca',
+    'invalido', 'invalido', 'peca',    'peca',    'peca',    'invalido', 'invalido',
+    'invalido', 'invalido', 'peca',    'peca',    'peca',    'invalido', 'invalido'
+];
+export function getTabuleiro() {
+    return [...tabuleiroResta1];
+}
